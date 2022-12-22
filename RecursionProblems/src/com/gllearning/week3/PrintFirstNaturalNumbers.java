0517182cf93dad986ea014b0ad0960a518b1c0a0
DIPANJAN DAS <das.dipanjan@hotmail.com>
2022-12-23 02:46:35 +0530

    Initial commit for DataStructureAlgorith repository.

---
+++ RecursionProblems/src/com/gllearning/week3/PrintFirstNaturalNumbers.java
@@ -0,0 +1,13 @@
+package com.gllearning.week3;
+
+import java.util.Scanner;
+
+public class PrintFirstNaturalNumbers {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please Enter Value of N.");
+
+	}
+
+}
